cd07bf1011818df152eebc2a623464b98078eb3d
Jaborg <jlappinsadiq@gmail.com>
2026-01-16 17:58:05 +0000

    Routing changes

--- backend/app/routes/books.py
+++ backend/app/routes/books.py
@@ -1,31 +1,34 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
-from app import crud, models, schemas
+from app import crud
 from app.database import get_db
+from app.models.book_model import Book
+from app.schemas.book_schema import BookCreate, BookOut
+from app.schemas.general_schema import CreateResponse
 
 router = APIRouter(prefix="/books", tags=["books"])
 
 
-@router.get("/", response_model=list[schemas.BookOut])
+@router.get("/", response_model=list[BookOut])
 async def read_root(member_id: int | None = None, db: Session = Depends(get_db)):
-    query = db.query(models.Book)
+    query = db.query(Book)
 
     if member_id is not None:
-        query = query.filter(models.Book.member_id.is_(member_id))
+        query = query.filter(Book.member_id.is_(member_id))
 
     return sorted(query.all(), key=lambda b: b.name)
 
 
-@router.get("/{book_id}", response_model=schemas.BookOut)
+@router.get("/{book_id}", response_model=BookOut)
 def get_book(book_id: int, db: Session = Depends(get_db)):
-    book = db.query(models.Book).filter(models.Book.id.is_(book_id).first())
+    book = db.query(Book).filter(Book.id.is_(book_id)).first()
     if not book:
         raise HTTPException(status_code=404, detail="Book not found")
     return book
 
 
-@router.post("/create_book", response_model=schemas.CreateResponse, status_code=201)
-async def create_book(book: schemas.BookCreate, db: Session = Depends(get_db)):
-    crud.create_all(db, models.Book, book)
+@router.post("/create_book", response_model=CreateResponse, status_code=201)
+async def create_book(book: BookCreate, db: Session = Depends(get_db)):
+    crud.create_all(db, Book, book)
     return {"message": "Books created successfully"}
--- backend/app/routes/members.py
+++ backend/app/routes/members.py
@@ -1,26 +1,30 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
-from app import crud, models, schemas
+from app import crud
 from app.database import get_db
+from app.models.member_model import Member
+from app.schemas.general_schema import CreateResponse
+from app.schemas.member_schema import MemberCreate, MemberOut
 
 router = APIRouter(prefix="/members", tags=["members"])
 
 
-@router.get("/", response_model=list[schemas.MemberOut])
+@router.get("/", response_model=list[MemberOut])
 async def read_root(db: Session = Depends(get_db)):
-    return sorted(crud.get_all(db, models.Member), key=lambda b: b.name)
+    return sorted(crud.get_all(db, Member), key=lambda b: b.name)
 
 
-@router.post("/create_member", response_model=schemas.CreateResponse, status_code=201)
-async def create_member(member: schemas.MemberCreate, db: Session = Depends(get_db)):
-    crud.create_all(db, models.Member, member)
-    return {"message": "Member created successfully"}
-
-
-@router.get("/{member_id}", response_model=schemas.MemberOut)
+@router.get("/{member_id}", response_model=MemberOut)
 def get_member(member_id: int, db: Session = Depends(get_db)):
-    member = db.query(models.Member).filter(models.Member.id.is_(member_id).first())
+    member = db.query(Member).filter(Member.id.is_(member_id)).first()
+    print("Member object looks like: ", member)
     if not member:
         raise HTTPException(status_code=404, detail="Member not found")
     return member
+
+
+@router.post("/create_member", response_model=CreateResponse, status_code=201)
+async def create_member(member: MemberCreate, db: Session = Depends(get_db)):
+    crud.create_all(db, Member, member)
+    return {"message": "Member created successfully"}
